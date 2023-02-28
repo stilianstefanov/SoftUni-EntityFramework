6d7ac5569cb1b7234a3fd60a2abd97583fe04e84
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-02-28 15:03:57 +0200

    Adding exercise

---
+++ 01. Entity Framework Introduction/08.Addresses by Town/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using SoftUni.Models;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContxt = new SoftUniContext();
+
+            Console.WriteLine(GetAddressesByTown(dbContxt));
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var addresses = context.Addresses
+                .Select(x => new
+                {
+                    x.AddressText,
+                    TownName = x.Town.Name,
+                    EmployeeCount = x.Employees.Count
+                })
+                .OrderByDescending(x => x.EmployeeCount)
+                .ThenBy(x => x.TownName)
+                .ThenBy(x => x.AddressText)
+                .Take(10)
+                .ToArray();
+
+            foreach ( var address in addresses )
+            {
+                sb.AppendLine($"{address.AddressText}, {address.TownName} - {address.EmployeeCount} employees");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
