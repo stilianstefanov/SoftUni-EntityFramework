1ee2ce4864c1e4f3c92a9a8b48ab1c3107fd7adf
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-02-28 15:42:10 +0200

    Adding exercise

---
+++ 01. Entity Framework Introduction/09.Employee 147/Models/Town.cs
@@ -0,0 +1,16 @@
+﻿namespace SoftUni.Models
+{
+    public partial class Town
+    {
+        public Town()
+        {
+            Addresses = new HashSet<Address>();
+        }
+
+        public int TownId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Address> Addresses { get; set; }
+    }
+}
---
+++ 01. Entity Framework Introduction/09.Employee 147/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using SoftUni.Models;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContxt = new SoftUniContext();
+
+            Console.WriteLine(GetEmployee147(dbContxt));
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employee = context.Employees
+                .Where(x => x.EmployeeId == 147)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    Projects = e.EmployeesProjects
+                                 .Select(ep => new
+                                 {
+                                     ProjectName = ep.Project.Name
+                                 })
+                                 .OrderBy(p => p.ProjectName)
+                                 .ToArray()
+                })
+                .FirstOrDefault();
+
+            sb.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+
+            sb.AppendLine(string.Join(Environment.NewLine, employee.Projects.Select(p => p.ProjectName)));
+          
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
