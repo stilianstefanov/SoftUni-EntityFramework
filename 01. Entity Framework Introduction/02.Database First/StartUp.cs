b3df0b05c706ef9d26b96bae2ea00e586e824815
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-02-28 11:49:47 +0200

    Initial commit

---
+++ 01. Entity Framework Introduction/02.Database First/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace SoftUni
+{
+    using Data;
+
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContxt= new SoftUniContext();
+            
+        }
+    }
+}
