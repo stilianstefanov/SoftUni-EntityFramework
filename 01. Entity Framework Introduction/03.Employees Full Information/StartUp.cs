09788143701db57cb94ca7b20a8f93ea1a4d8e00
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-02-28 12:15:50 +0200

    Adding exercises

---
+++ 01. Entity Framework Introduction/03.Employees Full Information/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContxt = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesFullInformation(dbContxt));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(x => new
+                {
+                    x.EmployeeId,
+                    x.FirstName,
+                    x.LastName,
+                    x.MiddleName,
+                    x.JobTitle,
+                    x.Salary
+                })
+                .OrderBy(x => x.EmployeeId)
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
