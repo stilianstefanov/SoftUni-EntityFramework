7a82b085540337d8366f0a6501c280e4901cf452
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-02-28 12:37:03 +0200

    Adding exercises

---
+++ 01. Entity Framework Introduction/05.Employees from Research and Development/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContxt = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(dbContxt));
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(x => new 
+                { 
+                    x.FirstName,
+                    x.LastName,
+                    DepartmentName = x.Department.Name,
+                    x.Salary
+                })
+                .Where(x => x.DepartmentName == "Research and Development")
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName)
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName} - ${employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
