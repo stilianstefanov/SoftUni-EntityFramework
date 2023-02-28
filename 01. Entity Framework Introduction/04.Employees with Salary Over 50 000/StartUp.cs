4357c1c8a069706d7dd914c7783a179f7462b499
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-02-28 12:26:52 +0200

    Adding exercises

---
+++ 01. Entity Framework Introduction/04.Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContxt = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(dbContxt));
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.Salary
+                })
+                .Where(x => x.Salary > 50000)
+                .OrderBy(x => x.FirstName)
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+            
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
