3a5645736a381c41f9efe88b23227b5945a0ab8f
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-01 11:06:35 +0200

    Adding exercise

---
+++ 01. Entity Framework Introduction/11.Find Latest 10 Projects/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using SoftUni.Models;
+    using System.Globalization;
+    using System.Text;
+    using System.Data;
+
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContxt = new SoftUniContext();
+
+
+            Console.WriteLine(GetLatestProjects(dbContxt));
+        }
+
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var projects = context.Projects
+                .OrderByDescending(p => p.StartDate)
+                .Take(10)
+                .Select(p => new
+                {
+                    p.Name,
+                    p.Description,
+                    p.StartDate
+                })
+                .ToList();
+
+            foreach (var project in projects.OrderBy(p => p.Name))
+            {
+                sb.AppendLine(project.Name);
+                sb.AppendLine(project.Description);
+                sb.AppendLine(project.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture));
+            }
+            
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
