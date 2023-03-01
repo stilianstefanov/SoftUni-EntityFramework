31669f313667578338f4f93be461295d25a02f13
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-01 11:22:53 +0200

    Adding exercise

---
+++ 01. Entity Framework Introduction/12.Increase Salaries/StartUp.cs
@@ -0,0 +1,57 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using SoftUni.Models;
+    using System.Globalization;
+    using System.Text;
+    using System.Data;
+
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext dbContxt = new SoftUniContext();
+
+
+            Console.WriteLine(IncreaseSalaries(dbContxt));
+        }
+
+        public static string IncreaseSalaries(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var allowableDeps = new string[]
+            {
+                "Engineering",
+                "Tool Design",
+                "Marketing",
+                "Information Services"
+            };
+
+            var employees = context.Employees
+                .Where(e => allowableDeps.Contains(e.Department.Name))
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .ToArray();
+
+            foreach (var emp in employees)
+            {
+                emp.Salary += emp.Salary * 0.12M;
+            }
+            
+            context.SaveChanges();
+
+
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.FirstName} {emp.LastName} (${emp.Salary:F2})");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+    }
+}
