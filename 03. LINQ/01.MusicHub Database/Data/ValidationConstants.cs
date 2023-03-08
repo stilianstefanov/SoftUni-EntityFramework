666f7bb946c86e172ec719879aa478b42bea9b88
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-08 19:22:18 +0200

    Initial commit

---
+++ 03. LINQ/01.MusicHub Database/Data/ValidationConstants.cs
@@ -0,0 +1,28 @@
+﻿namespace MusicHub.Data
+{
+    public static class ValidationConstants
+    {
+        //Song
+        public const int SongNameMaxLength = 20;
+
+        //Album
+        public const int AlbumNameMaxLength = 40;
+
+        //Performer
+        public const int PerformerFirstNameMaxLength = 20;
+
+        public const int PerformerLastNameMaxLength = 20;
+
+        //Producer
+        public const int ProducerNameMaxLength = 30;
+
+        public const int ProducerPseudonymMaxLength = 30;
+
+        public const int ProducerPhoneNumberMaxLength = 30;
+
+        //Writer
+        public const int WriterNameMaxLength = 20;
+
+        public const int WriterPseudonymMaxLength = 30;
+    }
+}
