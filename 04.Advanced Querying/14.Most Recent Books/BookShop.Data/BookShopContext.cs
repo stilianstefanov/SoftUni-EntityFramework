f776ab629030520a8e9d7c0d7641bac45d306cbc
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 20:02:12 +0200

    Adding exercise

---
+++ 04.Advanced Querying/14.Most Recent Books/BookShop.Data/BookShopContext.cs
@@ -0,0 +1,39 @@
+﻿namespace BookShop.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Models;
+    using EntityConfiguration;
+
+    public class BookShopContext : DbContext
+    {
+        public BookShopContext() { }
+
+        public BookShopContext(DbContextOptions options)
+        : base(options) { }
+
+        public DbSet<Book> Books { get; set; } = null!;
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        public DbSet<Author> Authors { get; set; } = null!;
+
+        public DbSet<BookCategory> BooksCategories { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new BookCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        }
+    }
+}
---
+++ 04.Advanced Querying/14.Most Recent Books/BookShop/StartUp.cs
@@ -0,0 +1,56 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetMostRecentBooks(db));
+        }
+
+        public static string GetMostRecentBooks(BookShopContext context)
+        {
+            var sb = new StringBuilder();
+
+            var categoriesInfo = context.Categories
+                .Select(c => new
+                {
+                    c.Name,
+                    RecentBooks = c.CategoryBooks
+                                      .OrderByDescending(cb => cb.Book.ReleaseDate)
+                                      .Take(3)
+                                      .Select(cb => new
+                                      {
+                                          BookName = cb.Book.Title,
+                                          ReleaseDate = cb.Book.ReleaseDate!.Value.Year
+                                      })
+                                      .ToArray()
+                })
+                .OrderBy(c => c.Name)
+                .ToArray();
+
+            foreach (var category in categoriesInfo)
+            {
+                sb.AppendLine($"--{category.Name}");
+
+                foreach (var book in category.RecentBooks)
+                {
+                    sb.AppendLine($"{book.BookName} ({book.ReleaseDate})");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
