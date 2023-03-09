daa1f5e28a9d791e5e1acf0b303cb76ee41e2c23
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 19:36:47 +0200

    Adding exercise

---
+++ 04.Advanced Querying/12.Total Book Copies/BookShop/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+                      
+            Console.WriteLine(CountCopiesByAuthor(db));
+        }
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            var sb = new StringBuilder();
+
+            var authorsInfo = context.Authors
+                .Select(a => new
+                {
+                    FullName = $"{a.FirstName} {a.LastName}",
+                    CopiesCount = a.Books.Sum(b => b.Copies)
+                })
+                .OrderByDescending(a => a.CopiesCount)
+                .ToArray();
+
+            foreach (var author in authorsInfo)
+            {
+                sb.AppendLine($"{author.FullName} - {author.CopiesCount}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
