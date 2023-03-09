5c9c2be18f27ea5ce2c9ce7d301b466755ab98ab
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 20:17:43 +0200

    Adding exercise

---
+++ 04.Advanced Querying/16.Remove Books/BookShop/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(RemoveBooks(db));
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var countOfDeletedBooks = context.Books
+                .Where(b => b.Copies < 4200)
+                .ToList();
+
+            context.RemoveRange(countOfDeletedBooks);
+
+            context.SaveChanges();
+
+            return countOfDeletedBooks.Count;
+        }
+    }
+}
+
+
