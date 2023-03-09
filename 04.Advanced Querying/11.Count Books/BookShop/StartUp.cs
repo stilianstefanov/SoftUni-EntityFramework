628dde7cfdb46581fa0562af7139a7df132dd35c
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 19:21:35 +0200

    Adding exercise

---
+++ 04.Advanced Querying/11.Count Books/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine()!;
+            
+            Console.WriteLine(CountBooks(db, int.Parse(input)));
+        }
+
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+        {
+            int countOfBooks = context.Books
+                .Where(b => b.Title.Length > lengthCheck)
+                .Count();
+
+            return countOfBooks;
+        }
+    }
+}
+
+
