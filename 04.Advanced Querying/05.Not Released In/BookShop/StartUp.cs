3897e950c173150e014d7ee2b1e0b4a520e1b89e
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 17:42:37 +0200

    Adding exercise

---
+++ 04.Advanced Querying/05.Not Released In/BookShop/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            int year = int.Parse(Console.ReadLine()!); 
+
+            Console.WriteLine(GetBooksNotReleasedIn(db, year));
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var booksInfo = context.Books
+                .Where(b => !b.ReleaseDate.HasValue || b.ReleaseDate.Value.Year != year)
+                .Select(b => new
+                {
+                    b.BookId,
+                    b.Title
+                })
+                .OrderBy(b => b.BookId)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, booksInfo.Select(b => b.Title));
+        }
+    }
+}
+
+
