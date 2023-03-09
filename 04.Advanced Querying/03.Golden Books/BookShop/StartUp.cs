dd75913e14dad01044b9ac2ecb733c956600c28e
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 17:14:08 +0200

    Adding exercise

---
+++ 04.Advanced Querying/03.Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetGoldenBooks(db));        
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+                .Select(b => new
+                {
+                    b.BookId,
+                    b.Title
+                })
+                .OrderBy(b => b.BookId)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, books.Select(b => b.Title));
+        }
+    }
+}
+
+
