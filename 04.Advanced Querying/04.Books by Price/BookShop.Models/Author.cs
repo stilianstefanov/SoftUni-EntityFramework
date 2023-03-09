05b595e551c7330cd83395f7894176a0a8764585
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 17:33:56 +0200

    Adding exercise

---
+++ 04.Advanced Querying/04.Books by Price/BookShop.Models/Author.cs
@@ -0,0 +1,20 @@
+﻿namespace BookShop.Models
+{
+    using System.Collections.Generic;
+
+    public class Author
+    {
+        public Author()
+        {
+            this.Books = new HashSet<Book>();
+        }
+
+        public int AuthorId { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public ICollection<Book> Books { get; set; } = null!;
+    }
+}
---
+++ 04.Advanced Querying/04.Books by Price/BookShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByPrice(db));    
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var sb = new StringBuilder();
+
+            var booksInfo = context.Books
+                .Where(b => b.Price > 40)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.Price
+                })
+                .OrderByDescending(b => b.Price)
+                .ToArray();
+
+            foreach (var book in booksInfo)
+            {
+                sb.AppendLine($"{book.Title} - ${book.Price:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
