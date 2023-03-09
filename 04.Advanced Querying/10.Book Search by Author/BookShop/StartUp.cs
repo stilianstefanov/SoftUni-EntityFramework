b0b652f5fbf619ad725e1cc84000a0d04a76d114
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 19:14:48 +0200

    Adding exercise

---
+++ 04.Advanced Querying/10.Book Search by Author/BookShop/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine()!;
+            
+            Console.WriteLine(GetBooksByAuthor(db, input));
+        }
+
+        public static string GetBooksByAuthor(BookShopContext context, string input)
+        {
+            var sb = new StringBuilder();
+
+            var booksInfo = context.Books
+                .Where(b => b.Author.LastName.ToLower().StartsWith(input.ToLower()))
+                .Select(b => new
+                {
+                    b.BookId,
+                    b.Title,
+                    Author = $"{b.Author.FirstName} {b.Author.LastName}"
+                })
+                .OrderBy(b => b.BookId)
+                .ToArray();
+
+            foreach (var book in booksInfo)
+            {
+                sb.AppendLine($"{book.Title} ({book.Author})");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
