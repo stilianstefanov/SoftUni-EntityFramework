90c44b082dba0a025d3875d0cc4a937aac978170
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 18:45:31 +0200

    Adding exercise

---
+++ 04.Advanced Querying/07.Released Before Date/BookShop/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine()!;
+            
+            Console.WriteLine(GetBooksReleasedBefore(db, input));
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var sb = new StringBuilder();
+
+            DateTime dateTime = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            var booksInfo = context.Books
+                .Where(b => b.ReleaseDate.HasValue && b.ReleaseDate < dateTime)
+                .OrderByDescending(b => b.ReleaseDate)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.EditionType,
+                    b.Price
+                })
+                .ToArray();
+
+            foreach (var book in booksInfo)
+            {
+                sb.AppendLine($"{book.Title} - {book.EditionType} - ${book.Price:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
