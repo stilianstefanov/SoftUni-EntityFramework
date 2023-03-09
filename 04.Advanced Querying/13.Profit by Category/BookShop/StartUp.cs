ca1e4bb0dd0866042cbd046d14d7943953c022f3
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 19:48:31 +0200

    Adding exercise

---
+++ 04.Advanced Querying/13.Profit by Category/BookShop/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+                      
+            Console.WriteLine(GetTotalProfitByCategory(db));
+        }
+
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            var sb = new StringBuilder();
+
+            var categoriesInfo = context.Categories
+                .Select(c => new
+                {
+                    c.Name,
+                    TotalProfit = c.CategoryBooks.Sum(cb => cb.Book.Copies * cb.Book.Price)
+                })
+                .OrderByDescending(c => c.TotalProfit)
+                .ThenBy(c => c.Name)
+                .ToArray();
+
+            foreach (var category in categoriesInfo)
+            {
+                sb.AppendLine($"{category.Name} ${category.TotalProfit:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
