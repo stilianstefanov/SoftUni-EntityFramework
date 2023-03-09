12ac7784c7f27356bcb618819e7e806e5d1abfb4
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2023-03-09 17:03:35 +0200

    Adding exercise

---
+++ 04.Advanced Querying/02.Age Restriction/BookShop.Models/BookCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Models
+{
+    public class BookCategory
+    {
+        public int BookId { get; set; }
+        public Book Book { get; set; } = null!;
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+    }
+}
---
+++ 04.Advanced Querying/02.Age Restriction/BookShop/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            string command = Console.ReadLine()!;
+
+            Console.WriteLine(GetBooksByAgeRestriction(db, command));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+            var booksInfo = context.Books                
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => new
+                {
+                    b.Title
+                })
+                .OrderBy(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, booksInfo.Select(b => b.Title));
+        }
+    }
+}
+
+
