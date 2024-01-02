5334dfafad8230439b19d2a8b31e8af50fff5271
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-01-02 12:45:33 +0200

    Add files via upload

---
+++ Exams/Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/Serializer.cs
@@ -0,0 +1,82 @@
+﻿namespace SoftJail.DataProcessor
+{
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.Data.Models;
+    using SoftJail.DataProcessor.ExportDto;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    public class Serializer
+    {
+        public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
+        {
+            var prisoners = context.Prisoners
+                .Where(p => ids.Contains(p.Id))
+                .ToArray()
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    CellNumber = p.Cell.CellNumber,
+                    Officers = p.PrisonerOfficers.Select(po => new
+                                                 {
+                                                     OfficerName = po.Officer.FullName,
+                                                     Department = po.Officer.Department.Name
+                                                 })
+                                                 .OrderBy(o => o.OfficerName)
+                                                 .ToArray(),              
+                    TotalOfficerSalary = Math.Round(p.PrisonerOfficers.Sum(po => po.Officer.Salary), 2)
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(prisoners, Formatting.Indented);
+        }
+
+        public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
+        {
+            string[] prNamesSplitted = prisonersNames.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            ExportPrisonerDto[] prisoners = context.Prisoners
+                .Where(p => prNamesSplitted.Contains(p.FullName))
+                .ToArray()
+                .Select(p => new ExportPrisonerDto()
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    IncarcerationDate = p.IncarcerationDate.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture),
+                    EncryptedMessages = p.Mails.Select(m => new ExportMessageDto()
+                    {
+                        Description = string.Join("", m.Description.Reverse().ToArray())
+                    })
+                    .ToArray()
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToArray();
+
+            return Serialize<ExportPrisonerDto[]>(prisoners, "Prisoners");
+        }
+
+        private static string Serialize<T>(T obj, string rootName)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            XmlRootAttribute xmlRoot =
+                new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer =
+                new XmlSerializer(typeof(T), xmlRoot);
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, obj, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
