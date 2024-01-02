863191fe440cee62f78e798becdc47e72eb2f044
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-01-02 12:44:41 +0200

    Add files via upload

---
+++ Exams/Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/ExportDto/ExportMessageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftJail.DataProcessor.ExportDto
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Message")]
+    public class ExportMessageDto
+    {
+        [XmlElement("Description")]
+        public string Description { get; set; } = null!;
+    }
+}
---
+++ Exams/Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/ExportDto/ExportPrisonerDto.cs
@@ -0,0 +1,20 @@
+﻿namespace SoftJail.DataProcessor.ExportDto
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Prisoner")]
+    public class ExportPrisonerDto
+    {
+        [XmlElement("Id")]
+        public int Id { get; set; }
+
+        [XmlElement("Name")]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("IncarcerationDate")]
+        public string IncarcerationDate { get; set; } = null!;
+
+        [XmlArray("EncryptedMessages")]
+        public ExportMessageDto[] EncryptedMessages { get; set; } = null!;
+    }
+}
