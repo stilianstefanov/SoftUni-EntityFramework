11735ddaa0a910409beff65f0d52b7b153690ad5
Stilian Stefanov <106154612+stilianstefanov@users.noreply.github.com>
2024-01-06 13:06:26 +0200

    Add files via upload

---
+++ Exams/Advanced Retake Exam - 16 Dec 2021/Artillery/DataProcessor/ExportDto/ExportGunDto.cs
@@ -0,0 +1,26 @@
+﻿namespace Artillery.DataProcessor.ExportDto
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Gun")]
+    public class ExportGunDto
+    {
+        [XmlAttribute("Manufacturer")]
+        public string Manufacturer { get; set; } = null!;
+
+        [XmlAttribute("GunType")]
+        public string GunType { get; set; } = null!;
+
+        [XmlAttribute("GunWeight")]
+        public string GunWeight { get; set; } = null!;
+
+        [XmlAttribute("BarrelLength")]
+        public string BarrelLength { get; set; } = null!;
+
+        [XmlAttribute("Range")]
+        public string Range { get; set; } = null!;
+
+        [XmlArray("Countries")]
+        public ExportCountryDto[] Countries { get; set; } = null!;
+    }
+}
